4df3e3dccf575b08e281cfec8a2e5675e4c9b9f4
Telmo Carmo <telmo.carmo@gmail.com>
2024-12-29 16:30:29 +0000

    1st commit

---
+++ rx_app1/rx_app1.py
@@ -0,0 +1,20 @@
+"""Welcome to Reflex!.
+
+reflex run --loglevel debug
+
+reflex run 
+
+"""
+
+# Import all the pages.
+from .pages import *
+from . import styles
+
+import reflex as rx
+
+
+# Create the app.
+app = rx.App(
+    style=styles.base_style,
+    stylesheets=styles.base_stylesheets,
+)
